dae0ace37df6058284dfaa3f4fd49a35b253a557
Daniela Brozzoni <danielabrozzoni@protonmail.com>
2020-08-02 17:58:55 +0200

    Read cities from file, fix regexp

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
 use curl::easy::Easy;
 use regex::Regex;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use tokio::time;
 
-async fn gather_data<'a>(url: &str) {
+fn gather_cities() -> Vec<String> {
+    let file = File::open("cities.txt").expect("Failed to open cities.txt");
+    let mut res = Vec::new();
+    for (index, line) in BufReader::new(file).lines().enumerate() {
+        if let Ok(l) = line {
+            res.push(l);
+        } else {
+            panic!("Failed to read line {}: {:?}", index - 1, line);
+        }
+    }
+    res
+}
+
+async fn gather_data(url: &str, cities: &Vec<String>) {
     let mut easy = Easy::new();
-    easy.url(url).unwrap();
+    easy.url(url).expect("Failed to parse URL");
+    // easy.transfer() allows write_function to capture stack-local data
+    let mut easy = easy.transfer();
+
     easy.write_function(move |data| {
         let text = html2text::from_read(data, 80);
-        let re: Regex = Regex::new(r"(?s)Brescia.*(?P<percentage>\d+,\d*)%").unwrap();
-        if let Some(c) = re.captures(&text) {
-            println!("{:?}", c.name("percentage").unwrap().as_str());
+        for city in cities {
+            let r = format!(r"(?s){}.*?\n(?P<percentage>\d+,*\d*)%\n", city);
+            let re: Regex = Regex::new(&r).expect("Failed to parse Regex");
+            if let Some(c) = re.captures(&text) {
+                if let Some(p) = c.name("percentage") {
+                    println!("{} {}", city, p.as_str());
+                }
+            }
         }
         Ok(data.len())
     })
@@ -20,9 +43,10 @@ async fn gather_data<'a>(url: &str) {
 #[tokio::main]
 async fn main() {
     let url = "https://srself.mcfit.com/disponibilita/disponibilita.aspx";
-    let mut interval = time::interval(time::Duration::from_secs(60*10));
+    let cities = gather_cities();
+    let mut interval = time::interval(time::Duration::from_secs(60 * 10));
     loop {
         interval.tick().await;
-        gather_data(&url).await;
+        gather_data(&url, &cities).await;
     }
 }
