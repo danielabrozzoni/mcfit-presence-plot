42a12eeeebc230949792bbe323b0715c14afbdb5
Daniela Brozzoni <danielabrozzoni@protonmail.com>
2020-08-02 18:23:50 +0200

    Print results to output file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use chrono::Utc;
 use curl::easy::Easy;
 use regex::Regex;
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader};
 use tokio::time;
+
+use std::io::Write;
 
 fn gather_cities() -> Vec<String> {
     let file = File::open("cities.txt").expect("Failed to open cities.txt");
@@ -17,7 +20,7 @@ fn gather_cities() -> Vec<String> {
     res
 }
 
-async fn gather_data(url: &str, cities: &Vec<String>) {
+async fn gather_data(url: &str, cities: &Vec<String>, outfile: &mut File) {
     let mut easy = Easy::new();
     easy.url(url).expect("Failed to parse URL");
     // easy.transfer() allows write_function to capture stack-local data
@@ -30,7 +33,14 @@ async fn gather_data(url: &str, cities: &Vec<String>) {
             let re: Regex = Regex::new(&r).expect("Failed to parse Regex");
             if let Some(c) = re.captures(&text) {
                 if let Some(p) = c.name("percentage") {
-                    println!("{} {}", city, p.as_str());
+                    write!(
+                        outfile,
+                        "{} - {} - {}\n",
+                        Utc::now().format("%a %e %b %Y %T"),
+                        city,
+                        p.as_str()
+                    )
+                    .unwrap();
                 }
             }
         }
@@ -44,9 +54,16 @@ async fn gather_data(url: &str, cities: &Vec<String>) {
 async fn main() {
     let url = "https://srself.mcfit.com/disponibilita/disponibilita.aspx";
     let cities = gather_cities();
+
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("results.txt")
+        .expect("Failed to open results.txt file");
+
     let mut interval = time::interval(time::Duration::from_secs(60 * 10));
     loop {
         interval.tick().await;
-        gather_data(&url, &cities).await;
+        gather_data(&url, &cities, &mut file).await;
     }
 }
